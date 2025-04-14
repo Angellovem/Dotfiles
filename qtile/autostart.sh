cba1c2b0ded21d1febfba736bab150bbff03263a
Angellovem <aeduardom27@gmail.com>
2025-04-14 11:30:23 -0500

    Qtile config working with basic setup

---
+++ qtile/autostart.sh
@@ -0,0 +1,4 @@
+!/bin/sh
+xrandr --output HDMI-1-0 --mode 1920x1080 --rate 120 --right-of eDP-1
+picom &
+feh --bg-scale ~/Wallpapers/pixelart_pokemon_rayquaza_forest_16x9.png ~/Wallpapers/pixelart_pokemon_rayquaza_forest_16x9.png &

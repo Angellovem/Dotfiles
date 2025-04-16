aea89bbb96f5a99c139fc045f4a40c3fd85db9a9
Angellovem <aeduardom27@gmail.com>
2025-04-15 23:02:47 -0500

    Fixed and documented qtile.config and made alacritty have rosepine theme

--- qtile/config.py
+++ qtile/config.py
@@ -1,34 +1,34 @@
-#=================================================================================================
-#       d8888                            888 d8b               .d88888b.  888    d8b 888          
-#      d88888                            888 88P              d88P" "Y88b 888    Y8P 888          
-#     d88P888                            888 8P               888     888 888        888          
-#    d88P 888 88888b.   .d88b.   .d88b.  888 "  .d8888b       888     888 888888 888 888  .d88b.  
-#   d88P  888 888 "88b d88P"88b d8P  Y8b 888    88K           888     888 888    888 888 d8P  Y8b 
-#  d88P   888 888  888 888  888 88888888 888    "Y8888b.      888 Y8b 888 888    888 888 88888888 
-# d8888888888 888  888 Y88b 888 Y8b.     888         X88      Y88b.Y8b88P Y88b.  888 888 Y8b.     
-#d88P     888 888  888  "Y88888  "Y8888  888     88888P'       "Y888888"   "Y888 888 888  "Y8888  
-#                           888                                      Y8b                          
-#                      Y8b d88P                                                                   
-#                       "Y88P"                                                                    
-#=================================================================================================
+# Color dictionary
+colors = {
+    "bg0": "#191724",  # base
+    "bg1": "#1f1d2e",  # surface
+    "fg0": "#e0def4",  # text
+    "accent_color": "#eb6f92",  # love
+    "urgent_color": "#f6c177",  # gold
+    "bg": "#232136",  # background
+    "red": "#eb6f92",  # accent
+    "accent": "#c4a7e7",  # accent
+    "subtle": "#393552"  # subtle
+}
+
+# Global settings
+font = "RobotoMono Nerd Font Mono 16"
+background_color = "transparent"
+text_color = colors["fg0"]
+margin = 0
+padding = 0
+spacing = 0
+
+# Import Qtile libraries
 from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 from libqtile import hook
-
-# .d8888b.  888                     888    
-#d88P  Y88b 888                     888    
-#Y88b.      888                     888    
-# "Y888b.   888888  8888b.  888d888 888888 
-#    "Y88b. 888        "88b 888P"   888    
-#      "888 888    .d888888 888     888    
-#Y88b  d88P Y88b.  888  888 888     Y88b.  
-# "Y8888P"   "Y888 "Y888888 888      "Y888 
-
 import os
 import subprocess
 
+# Autostart hook
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
@@ -37,18 +37,7 @@ def autostart():
 mod = "mod4"
 terminal = guess_terminal()
 
-#888    d8P                             
-#888   d8P                              
-#888  d8P                               
-#888d88K      .d88b.  888  888 .d8888b  
-#8888888b    d8P  Y8b 888  888 88K      
-#888  Y88b   88888888 888  888 "Y8888b. 
-#888   Y88b  Y8b.     Y88b 888      X88 
-#888    Y88b  "Y8888   "Y88888  88888P' 
-#                          888          
-#                     Y8b d88P          
-#                      "Y88P"           
-
+# Key bindings
 keys = [
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -92,6 +81,7 @@ keys = [
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
+# Groups 
 groups = [Group(i) for i in "123456789"]
 
 for i in groups:
@@ -110,63 +100,27 @@ for i in groups:
         ]
     )
 
-#888                                          888    
-#888                                          888    
-#888                                          888    
-#888       8888b.  888  888  .d88b.  888  888 888888 
-#888          "88b 888  888 d88""88b 888  888 888    
-#888      .d888888 888  888 888  888 888  888 888    
-#888      888  888 Y88b 888 Y88..88P Y88b 888 Y88b.  
-#88888888 "Y888888  "Y88888  "Y88P"   "Y88888  "Y888 
-#                       888                          
-#                  Y8b d88P                          
-#                   "Y88P"                           
-
+# Layout theme
 layout_theme = {
     "border_width": 3,
     "margin": 15,
-    "border_focus": "#ebbcba",
+    "border_focus": colors["accent_color"],
     "border_normal": "#44415a"
 }
 
+# Layout
 layouts = [
-    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    # layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
     layout.MonadTall(**layout_theme),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(**layout_theme),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
-
-# Drag floating layouts.
+# Drag floating layouts
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
-
-
-#888       888 d8b      888                   888    
-#888   o   888 Y8P      888                   888    
-#888  d8b  888          888                   888    
-#888 d888b 888 888  .d88888  .d88b.   .d88b.  888888 
-#888d88888b888 888 d88" 888 d88P"88b d8P  Y8b 888    
-#88888P Y88888 888 888  888 888  888 88888888 888    
-#8888P   Y8888 888 Y88b 888 Y88b 888 Y8b.     Y88b.  
-#888P     Y888 888  "Y88888  "Y88888  "Y8888   "Y888 
-#                                888                 
-#                           Y8b d88P                 
-#                            "Y88P"                  
-
+# Widget defaults
 widget_defaults = dict(
     font="RobotoMono Nerd Font Mono",
     fontsize=14,
@@ -174,19 +128,12 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-colors = {
-    "bg": "#232136",
-    "fg": "#e0def4",
-    "red": "#eb6f92",
-    "accent": "#c4a7e7",
-    "subtle": "#393552"
-}
-
+# Left, center, and right widgets
 def left_widgets():
     return [
         widget.GroupBox(
             highlight_method="line",
-            active=colors["fg"],
+            active=colors["fg0"],
             inactive=colors["subtle"],
             this_current_screen_border=colors["accent"],
             other_screen_border=colors["red"],
@@ -200,7 +147,7 @@ def center_widgets():
         widget.Spacer(length=bar.STRETCH),
         widget.Clock(
             format="%Y-%m-%d (%a) %I:%M %p",
-            foreground=colors["fg"]
+            foreground=colors["fg0"]
         ),
         widget.Spacer(length=bar.STRETCH),
     ]
@@ -215,7 +162,7 @@ def right_widgets():
         widget.GenPollText(
             update_interval=1800,
             func=lambda: "Bogotá~17°C",
-            foreground=colors["fg"]
+            foreground=colors["fg0"]
         ),
         widget.Sep(linewidth=0, padding=6),
         widget.Battery(
@@ -223,20 +170,21 @@ def right_widgets():
             charge_char="⚡",
             discharge_char="🔋",
             full_char="✔️",
-            foreground=colors["fg"]
+            foreground=colors["fg0"]
         ),
     ]
 
 def default_widgets():
     return left_widgets() + center_widgets() + right_widgets()
 
+# Screens configuration
 screens = [
     Screen(
         top=bar.Bar(
             default_widgets(),
             24,
             background=colors["bg"],
-            opacity=0.95,
+            opacity=0.8,
             margin=[4, 8, 0, 8],
         )
     ), 
@@ -245,29 +193,15 @@ screens = [
             default_widgets(),
             24,
             background=colors["bg"],
-            opacity=0.95,
+            opacity=0.8,
             margin=[4, 8, 0, 8],
         )
     )
 ]
-# .d8888b.  888          888               888 
-#d88P  Y88b 888          888               888 
-#888    888 888          888               888 
-#888        888  .d88b.  88888b.   8888b.  888 
-#888  88888 888 d88""88b 888 "88b     "88b 888 
-#888    888 888 888  888 888  888 .d888888 888 
-#Y88b  d88P 888 Y88..88P 888 d88P 888  888 888 
-# "Y8888P88 888  "Y88P"  88888P"  "Y888888 888 
 
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-floats_kept_above = True
-cursor_warp = False
+# Floating layout settings
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -278,6 +212,7 @@ floating_layout = layout.Floating(
     ]
 )
 
+# Qtile configurations
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
@@ -289,4 +224,5 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-wmname = "LG3D"
+wmname = "Angel's"
+
